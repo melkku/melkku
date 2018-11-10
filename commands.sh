b7dc435458ee1ac77e265c1d17a1a1c95c07ab3a
melkku <chienlin.w@gmail.com>
2018-11-11 07:59:20 +1100

    Fixed Staking

--- commands.sh
+++ commands.sh
@@ -3,24 +3,24 @@
 clear
 
 # List the selection options
-echo ~~~~ Welcome to easy select ~~~ 
+echo ~~~~ Welcome to easy select ~~~
 echo 1. Check staking status
 echo 2. Unlock wallet
-echo 3. List useful commands 
-echo 
+echo 3. List useful commands
+echo
 
 # Checking Selection
 read -p "What is your selection: " selection
 if [ "$selection" == "1"  ]; then
    clear
-   echo ~~~ Displaying SwiftCash Staking Status ~~~
    ~/GIT/STAKING/stakingSelection.sh
+elif [ "$selection" == "2" ]; then
+   clear
+   ~/GIT/UNLOCK/unlockSelection.sh
 elif [ "$selection" == "3" ]; then
    clear
    ~/GIT/OTHER/usefulcommands.sh
 else
    echo Selection $selection not found...
 fi
-echo 
-
-
+echo
